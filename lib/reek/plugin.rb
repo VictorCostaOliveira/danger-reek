997b0cc80b10ec31f9486da60e46d3f8ff0ef449
Victor Costa <victorcosta@jera.com.br>
2019-07-30 14:58:12 -0400

    remove raise

--- lib/reek/plugin.rb
+++ lib/reek/plugin.rb
@@ -33,7 +33,6 @@ module Danger
     end
 
     def fetch_files_to_lint(files)
-      raise "Error"
       to_lint = (files ? Dir.glob(files) : (git.modified_files + git.added_files))
       ::Reek::Source::SourceLocator.new(to_lint).sources
     end
