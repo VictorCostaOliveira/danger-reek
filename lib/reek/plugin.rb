47e29d1bd5be901f6a150d8eb494c92feb89cc75
Victor Costa <victorcosta@jera.com.br>
2019-07-30 12:18:06 -0400

    adding valition to receive personalized files to analyze

--- lib/reek/plugin.rb
+++ lib/reek/plugin.rb
@@ -17,7 +17,7 @@ module Danger
     # Runs Ruby files through Reek.
     # @return [Array<Reek::SmellWarning, nil>]
     def lint(files = nil ,reek_rules_path = nil)
-      files_to_lint = files || fetch_files_to_lint
+      files_to_lint = fetch_files_to_lint(files)
       code_smells   = run_linter(files_to_lint, reek_rules_path)
       warn_each_line(code_smells)
     end
@@ -32,9 +32,11 @@ module Danger
       end
     end
 
-    def fetch_files_to_lint
-      files = git.modified_files + git.added_files
-      ::Reek::Source::SourceLocator.new(files).sources
+    def fetch_files_to_lint(files)
+      unless files.present?
+        files = git.modified_files + git.added_files
+      end
+      reek_source_locator(files)
     end
 
     def warn_each_line(code_smells)
@@ -45,6 +47,10 @@ module Danger
           warn(message, file: source, line: line)
         end
       end
+    end
+
+    def reek_source_locator(files)
+      ::Reek::Source::SourceLocator.new(files).sources
     end
   end
 end
